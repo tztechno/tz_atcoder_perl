f80a50fa2bfbe4d0c9e0bf2a64e3b0d83efeba3e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-02 14:40:19 +0900

    Update pressure.pl

--- pressure.pl
+++ pressure.pl
@@ -1,18 +1,14 @@
 use strict;
 use warnings;
-
 my $input = <STDIN>;
 chomp($input);
-
 my $D = 0;
 eval {
     $D = int($input);
 };
-
 if ($@) {
     warn "Invalid input. Please enter an integer.\n";
     exit 1;
 }
-
 my $ans = $D / 100;
 print "$ans\n";
