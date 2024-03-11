d76cea3d74a5b1fb6cd86bdeccecbeaa94e7d1d3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-11 18:18:51 +0900

    Create abc166_a.pl

---
+++ abc166_a.pl
@@ -0,0 +1,37 @@
+//abc166_a.pl
+################################
+################################
+################################
+#!usr/bin/perl
+use strict;
+use warnings;
+my $str = <STDIN>;
+chomp($str);
+if($str eq "ABC"){
+    print "ARC\n";
+} else {
+    print "ABC\n";
+}
+################################
+print<>=~/B/?ARC:ABC
+################################
+my $s = <>;
+chomp $s;
+print($s eq 'ABC' ? 'ARC' : 'ABC');
+################################
+my $s = <STDIN>;
+chomp $s;
+if( $s eq "ABC"){
+    print "ARC";
+}else{
+    print "ABC";
+}
+################################
+my $S = <STDIN>;
+chomp($S);
+if ($S eq "ABC") {
+    print "ARC\n";
+} elsif ($S eq "ARC") {
+    print "ABC\n";
+}
+################################
